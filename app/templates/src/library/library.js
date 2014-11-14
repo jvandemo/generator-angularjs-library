bfa796cba78a95cdf09e878b785964b92752a04a
Steven Ceuppens <steven.ceuppens@icloud.com>
2014-11-14 11:34:15 +0100

    Typo Gulp instead of Grunt

--- app/templates/src/library/library.js
+++ app/templates/src/library/library.js
@@ -1,6 +1,6 @@
 // Create all modules and define dependencies to make sure they exist
 // and are loaded in the correct order to satisfy dependency injection
-// before all nested files are concatenated by Grunt
+// before all nested files are concatenated by Gulp
 
 // Config
 angular.module('<%= config.libraryName.camelized %>.config', [])
